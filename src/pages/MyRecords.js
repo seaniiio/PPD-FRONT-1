062e2b4e62099ee21c28288f435f901fcbc8e2cd
seaniiio <siwon@gimsiwon-ui-MacBookPro.local>
2023-05-24 01:35:30 +0900

    [FEAT] #5 MainPage 작업과 styled-components 작업

---
+++ src/pages/MyRecords.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import Top from '../components/Top'
+
+
+function MyRecords() {
+    return (
+        <div>
+            <Top state='visible'></Top>
+            <p>보행기록 페이지</p>
+        </div>
+    )
+}
+
+export default MyRecords;
