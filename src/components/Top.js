50c9ec53f07243a03462390624caeb6141db48ad
seaniiio <siwon@gimsiwon-ui-MacBookPro.local>
2023-05-24 10:43:06 +0900

    [Design] #7 순수css를 styled-components로 모두 변경 및 button design 수정

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,10 +1,27 @@
 // 상단바에 대한 컴포넌트
 import '../App.css';
+import styled from 'styled-components';
+
+const TopBar = styled.div `
+  background-color:lightgray;
+  height:60px;
+`
+
+const InvisibleTopBar = styled(TopBar) `
+  background-color:white;
+`
+
 const Top = (props) => {
-    if(props.state === 'visible') { // prop으로 visible을 입력받으면 회색으로 표시된다.
-      return <div className="top"></div>
-    } else if (props.state === 'invisible') { //prop으로 invisible을 입력받으면 공간차지만 하고 보이지는 않는다
-      return <div className="top_invisible"></div>
+  // prop으로 visible을 입력받으면 회색으로 표시된다.
+    if(props.state === 'visible') { 
+      return (
+        <TopBar></TopBar>
+      )
+  // prop으로 invisible을 입력받으면 공간차지만 하고 보이지는 않는다
+    } else if (props.state === 'invisible') { 
+      return (
+        <InvisibleTopBar></InvisibleTopBar>
+      )
     }
   }
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,7 +6,12 @@ import Measure from './Measure'
 import MyInformation from './MyInformation';
 import MyRecords from './MyRecords';
 import {Link} from 'react-router-dom'
+import styled from 'styled-components';
 
+//내정보, 보행기록 두 개의 버튼을 하나로 묶어서 중앙정렬하기위해 생성한 컴포넌트
+const ToCenter = styled.div `
+  text-align:center;
+`
 //메인페이지
 function MainPage() {
     return (
@@ -14,10 +19,10 @@ function MainPage() {
         <Top state='visible'></Top>
         <Center img='person'></Center>
         <Link to="/Measure"><Button name="측정 시작" show="big"></Button></Link>
-        <div className="center">
+        <ToCenter>
         <Link to="/MyInfo"><Button name="내 정보" show="many"></Button></Link>
         <Link to="/MyRecords"><Button name="보행 기록" show="many"></Button></Link>
-        </div>
+        </ToCenter>
       </div>
     )
 }
