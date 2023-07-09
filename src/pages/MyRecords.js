f5dc3486ef3edb3c10efce039d5f6a71e5ecdd0f
차현우 <chahyeonwoo@chahyeon-us-MacBook-Air.local>
2023-07-09 22:50:34 +0900

    보행기록 페이지 구현

--- src/pages/MyRecords.js
+++ src/pages/MyRecords.js
@@ -1,14 +1,20 @@
-import '../App.css';
+import React from 'react';
+import Square from '../components/Square';
+import personIcon from '../images/icon_person.png';
 import Top from '../components/Top'
 
-
-function MyRecords() {
-    return (
-        <div>
-            <Top state='visible'></Top>
-            <p>보행기록 페이지</p>
+const MyRecords = () => {
+  return (
+    <div>
+        <Top state='visible' text='보행기록'></Top>
+        <div className="square-container">
+        <Square color="#cce5ff" imageUrl={personIcon} buttonText="보행기록 1" />
+        <Square color="#cce5ff" imageUrl={personIcon} buttonText="보행기록 2" />
+        <Square color="#cce5ff" imageUrl={personIcon} buttonText="보행기록 3" />
+        <Square color="#ffd6e5" imageUrl={personIcon} buttonText="보행기록 4" />
         </div>
-    )
-}
+    </div>
+  );
+};
 
 export default MyRecords;
