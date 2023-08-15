593c11b7f25350f6f1098f23b6d2171f75ef5ad2
juya <kjy2967@naver.com>
2023-08-16 07:02:42 +0900

    [Refactor]: #59 response 변경 & result 객체 이동 변경

--- src/pages/Abnormal.js
+++ src/pages/Abnormal.js
@@ -1,23 +1,42 @@
-import '../App.css';
+import '../App.css'
 import Top from '../components/Top'
 import Button from '../components/Button'
 import '../styles/Loading.css'
-import {Link, useNavigate} from 'react-router-dom';
-
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 function Abnormal() {
-    return (
-        <div>
-            <Top state='visible'></Top>
-            <div className="textShow">당신의 보행은 <span style={{ color: "red" }}>비정상</span>입니다.</div>
-            <div className="svg-loader">
-            <svg className="svg-container" height="100" width="100" viewBox="0 0 100 100">
-                <circle className="loader-svg bg red" cx="50" cy="50" r="45"></circle>
-            </svg>
-            </div>
-            <Link to="/Result"><Button name="결과 확인" show="bottom2" style={{ paddingTop: "200px" }}></Button></Link>
-        </div>
-    )
+  const location = useLocation()
+  const [record, setRecord] = useState(location.state && location.state.record)
+
+  return (
+    <div>
+      <Top state="visible"></Top>
+      <div className="textShow">
+        당신의 보행은 <span style={{ color: 'red' }}>비정상</span>입니다.
+      </div>
+      <div className="svg-loader">
+        <svg
+          className="svg-container"
+          height="100"
+          width="100"
+          viewBox="0 0 100 100"
+        >
+          <circle className="loader-svg bg red" cx="50" cy="50" r="45"></circle>
+        </svg>
+      </div>
+      <Link
+        to={`/Result/${record.idx}`}
+        state={{ record: record }} // s 객체를 state로 전달
+      >
+        <Button
+          name="결과 확인"
+          show="bottom2"
+          style={{ paddingTop: '200px' }}
+        ></Button>
+      </Link>
+    </div>
+  )
 }
 
-export default Abnormal;
+export default Abnormal
