719aa184910f6984e6fb9bf6a2b8877b72fedc88
seaniiio <cnc4750@naver.com>
2023-08-17 21:22:50 +0900

    feat: 결과 상세 페이지 구현

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -81,6 +81,8 @@ function Result() {
         })
     }
 
+    const navigate = useNavigate();
+
   return (
     <>
       <Top state="visible" text="분석결과" home="true"></Top>
@@ -107,7 +109,7 @@ function Result() {
         <SaveButton onClick={saveRecord}>기록 저장하기</SaveButton>
       </>
 
-      <StyledLink
+      <StyledLink data={record}
         to={record.resultTypes[0] === 1 ? '/ResultDetail' : undefined}
       >
         <FeatureResult
@@ -116,7 +118,7 @@ function Result() {
           속도
         </FeatureResult>
       </StyledLink>
-      <StyledLink
+      <StyledLink data={record}
         to={record.resultTypes[1] === 1 ? '/ResultDetail' : undefined}
       >
         <FeatureResult
@@ -125,7 +127,7 @@ function Result() {
           발목 사이 거리
         </FeatureResult>
       </StyledLink>
-      <StyledLink
+      <StyledLink data={record}
         to={record.resultTypes[2] === 1 ? '/ResultDetail' : undefined}
       >
         <FeatureResult
@@ -134,7 +136,7 @@ function Result() {
           무릎 사이 거리
         </FeatureResult>
       </StyledLink>
-      <StyledLink
+      <StyledLink data={record}
         to={record.resultTypes[3] === 1 ? '/ResultDetail' : undefined}
       >
         <FeatureResult
@@ -143,7 +145,7 @@ function Result() {
           무릎 각도
         </FeatureResult>
       </StyledLink>
-      <StyledLink
+      <StyledLink data={record}
         to={record.resultTypes[4] === 1 ? '/ResultDetail' : undefined}
       >
         <FeatureResult
@@ -152,7 +154,7 @@ function Result() {
           팔꿈치 각도
         </FeatureResult>
       </StyledLink>
-      <StyledLink
+      <StyledLink data={record}
         to={record.resultTypes[5] === 1 ? '/ResultDetail' : undefined}
       >
         <FeatureResult
--- src/pages/ResultDetail.js
+++ src/pages/ResultDetail.js
@@ -3,27 +3,191 @@ import Top from '../components/Top'
 import Button from '../components/Button'
 import styled from 'styled-components';
 import '../styles/Loading.css'
-import {Link, useNavigate} from 'react-router-dom';
+import {Link, useNavigate, useLocation} from 'react-router-dom';
 import featureImg from '../images/feature_1.png'
+import Chart from "react-apexcharts";
+import ApexCharts from "react-apexcharts";
+import ReactApexChart from "react-apexcharts";
+import '../styles/Graph.css'
 
-const DetailBox = styled.div `
-    background-color: #bad5f7;
 
-    padding: 10px;
-    margin: 20px;
-    height: 300px;
-
+const GraphSpace = styled.div`
+    display: inline-block;
+    width: 190px;
     text-align: center;
-`;
+    margin-top: 30px;
+`
+
+const testData = 
+    {
+        result : 0,
+        normalValues : [
+            1.5,
+            2.5,
+            2,
+            4,
+            1,
+            3.3
+        ],
+        abnormalValues : [
+            3,
+            6,
+            4,
+            1,
+            3,
+            1
+        ],
+        actualValues : [
+            2,
+            2.6,
+            3,
+            6,
+            2.5,
+            2.5
+        ],
+        resultTypes : [
+            0,
+            0,
+            1,
+            0,
+            1,
+            0
+        ]
+    }
+
+// 퍼센트로 바꿔서 배열로 return
+const toPercent = (e) => {
+    const actual = testData.actualValues[e];
+    const normal = testData.normalValues[e];
+    const abnormal = testData.abnormalValues[e];
+    const max = Math.max(actual, normal, abnormal);
+
+    return [(normal / max) * 100, (actual / max) * 100, (abnormal / max) * 100]
+}
+
+const chart = {
+    options: {
+        chart: {
+        height: 200,
+        type: 'radialBar',
+        },
+        plotOptions: {
+        radialBar: {
+            offsetY: 0,
+            startAngle: 0,
+            endAngle: 270,
+            hollow: {
+                margin: 5,
+                size: '5%',
+                background: 'transparent',
+                image: undefined,
+            },
+            dataLabels: {
+                name: {
+                    show: false,
+                },
+                value: {
+                    show: false,
+                }
+            }
+        }
+        },
+        colors: ['#6b9ccf', '#7d7c7c', '#cf7897'],
+        labels: ['정상', '결과', '비정상'],
+        responsive: [{
+            breakpoint: 480,
+            options: {
+            }
+        }]
+    }
+}
 
 
 function ResultDetail() {
+    //const location = useLocation()
+
     return (
         <div>
             <Top state='visible' text="분석결과" home="true" back="true"></Top>
-            <img src={featureImg} style={{width: "400px"}} ></img>
-            <DetailBox>이러이러해서 비정상입니다.~~~~~~~~~~~~~~~~~~~~~~~~~~`</DetailBox>
+
+            <GraphSpace>
+                <ReactApexChart 
+                    options={chart.options}
+                    series={toPercent(0)}
+                    type="radialBar" 
+                    width="210"
+                />
+                <span class={testData.resultTypes[0] === 0 ? 
+                "normalhighlight" :"abnormalhighlight"}>보행 속도</span>
+            </GraphSpace>
+
+            <GraphSpace>  
+                <ReactApexChart 
+                    options={chart.options}
+                    series={toPercent(1)}
+                    type="radialBar" 
+                    width="210"
+                />
+                <span class={testData.resultTypes[1] === 0 ? 
+                "normalhighlight" :"abnormalhighlight"}>발목 사이 거리</span>
+            </GraphSpace>
+
+            <GraphSpace>
+                <ReactApexChart 
+                    options={chart.options}
+                    series={toPercent(2)}
+                    type="radialBar" 
+                    width="210"
+                />
+                <span class={testData.resultTypes[2] === 0 ? 
+                "normalhighlight" :"abnormalhighlight"}>무릎 사이 거리</span>
+            </GraphSpace>
+
+            <GraphSpace>
+                <ReactApexChart 
+                    options={chart.options}
+                    series={toPercent(3)}
+                    type="radialBar" 
+                    width="210"
+                />
+                <span class={testData.resultTypes[3] === 0 ? 
+                "normalhighlight" :"abnormalhighlight"}>무릎 각도</span>
+            </GraphSpace>
+
+            <GraphSpace>
+                <ReactApexChart 
+                    options={chart.options}
+                    series={toPercent(4)}
+                    type="radialBar" 
+                    width="210"
+                />  
+                <span class={testData.resultTypes[4] === 0 ? 
+                "normalhighlight" :"abnormalhighlight"}>팔꿈치 각도</span>
+            </GraphSpace>
+
+            <GraphSpace>
+                <ReactApexChart 
+                    options={chart.options}
+                    series={toPercent(5)}
+                    type="radialBar" 
+                    width="210"
+                />
+                <span class={testData.resultTypes[5] === 0 ? 
+                "normalhighlight" :"abnormalhighlight"}>허리 각도</span>
+            </GraphSpace>
+            
+            <div class="label">
+
+                    <span class="normalhighlight">정상 평균</span>
+                    <br/>
+                    <span class="abnormalhighlight">병적 평균</span>
+                    <br/>
+                    <span class="myhighlight">나의 결음 분석 결과</span>
+
+            </div>
+
         </div>
+
     )
 }
 
