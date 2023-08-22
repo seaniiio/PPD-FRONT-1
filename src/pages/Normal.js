ce82081f9d9782e56b48c2247eed7cb6fa431c95
seaniiio <cnc4750@naver.com>
2023-08-22 17:07:20 +0900

    design: #65 정상결과, 비정상결과 디자인 수정

--- src/pages/Normal.js
+++ src/pages/Normal.js
@@ -1,9 +1,15 @@
 import '../App.css'
-import Top from '../components/Top'
-import Button from '../components/Button'
+import TopBar from '../components/TopBar'
+import {Button, BlueButton, BlueButtonContainer} from '../components/Button'
 import '../styles/Loading.css'
 import { Link, useLocation} from 'react-router-dom'
 import { useState } from 'react'
+import {HiMagnifyingGlass} from 'react-icons/hi2'
+import styled from 'styled-components'
+
+const ButtonContainer = styled(BlueButtonContainer) `
+  margin-top: 400px;
+`
 
 function Normal() {
   const location = useLocation()
@@ -12,9 +18,9 @@ function Normal() {
   return (
 
     <div>
-      <Top state="visible"></Top>
+      <TopBar text="분석결과"></TopBar>
       <div className="textShow">
-        당신의 보행은 <span style={{ color: 'blue' }}>정상</span>입니다.
+        당신의 보행은 <span style={{ color: 'blue', "fontWeight" : "bolder" }}>정상</span>입니다.
       </div>
       <div className="svg-loader">
         <svg
@@ -26,16 +32,16 @@ function Normal() {
           <circle className="loader-svg bg" cx="50" cy="50" r="45"></circle>
         </svg>
       </div>
-      <Link
-        to={'/Result'}
-        state={{ record: record }} // s 객체를 state로 전달
-      >
-        <Button
-          name="결과 확인"
-          show="bottom2"
-          style={{ paddingTop: '200px' }}
-        ></Button>
-      </Link>
+      
+      <ButtonContainer>
+        <Link
+          to={'/Result'}
+          state={{ record: record }} // s 객체를 state로 전달
+        >
+          <BlueButton><HiMagnifyingGlass style={{"marginRight":"4px"}}/>결과확인</BlueButton>
+        </Link>
+      </ButtonContainer>
+      
     </div>
   )
 }
